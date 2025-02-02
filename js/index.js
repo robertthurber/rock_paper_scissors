0102d083c6b3070853c36c5f5b0e584c1eb0d0bb
Robert <83424654+robertthurber@users.noreply.github.com>
2025-02-02 16:31:10 -0500

    finished the project add the game rules function with playing game function

--- js/index.js
+++ js/index.js
@@ -1,3 +1,7 @@
+//creating variables
+let humanScore = 0;
+let computerScore = 0;
+
 // creating a function getting the computer choice
 function getComputerChoice() {
   let randomNumber = Math.floor(Math.random() * 3);
@@ -5,12 +9,15 @@ function getComputerChoice() {
   switch (randomNumber) {
     case 0:
       computerChoice = "Rock";
+      console.log("Computer Selected Rock");
       break;
     case 1:
       computerChoice = "Paper";
+      console.log("Computer Selected Paper");
       break;
     case 2:
       computerChoice = "Scissors";
+      console.log(" Computer Selected Scissors");
       break;
     default:
       console.log("Something went wrong");
@@ -21,12 +28,54 @@ function getComputerChoice() {
 //creating a function getting the human choice. Prompts users to enter a choice. make sure string is in correct formate
 function getHumanChoice() {
   let userChoice = prompt("Choose: Rock, Paper, or Scissors");
+  //here we are just taking what the user enters and converting it to the style that i need for checking, not making sure the user enters correct thing
   let firstCharUserChoice = userChoice.charAt(0).toUpperCase();
   let restOfString = userChoice.substring(1).toLowerCase();
   let humanSelection = firstCharUserChoice + restOfString;
   return humanSelection;
 }
 
-getHumanChoice();
+// creating a function that will determine the winner and add to the score
 
-console.log(getHumanChoice());
+function playRound(humanChoice, computerChoice) {
+  const humanSelection = getHumanChoice();
+  const computerSelection = getComputerChoice();
+  if (humanSelection == "Rock" && computerSelection == "Rock") {
+    console.log("It's a Tie");
+  } else if (humanSelection == "Rock" && computerSelection == "Paper") {
+    console.log("Computer Wins!");
+    computerScore += 1;
+  } else if (humanSelection == "Rock" && computerSelection == "Scissors") {
+    console.log("You Wins!");
+    humanScore += 1;
+  } else if (humanSelection == "Paper" && computerSelection == "Paper") {
+    console.log("It's a Tie");
+  } else if (humanSelection == "Paper" && computerSelection == "Rock") {
+    console.log("You Win!");
+    humanScore += 1;
+  } else if (humanSelection == "Paper" && computerSelection == "Scissors") {
+    console.log("Computer Wins!");
+    computerScore += 1;
+  } else if (humanSelection == "Scissors" && computerSelection == "Scissors") {
+    console.log("It's a Tie");
+  } else if (humanSelection == "Scissors" && computerSelection == "Rock") {
+    console.log("Computer Wins!");
+    computerScore += 1;
+  } else if (humanSelection == "Scissors" && computerSelection == "Paper") {
+    console.log("You Win!");
+    humanScore += 1;
+  } else {
+    console.log("Something went wrong");
+  }
+}
+function playGame() {
+  let i = 0;
+  while (i <= 5) {
+    playRound();
+    console.log("Computer score is: " + computerScore);
+    console.log("Human score is: " + humanScore);
+    i++;
+  }
+}
+
+playGame();
